32f2687453b79afd8cb0073003a52820dd0a6069
MudeDineshNaik <mudedineshnaik7@gmail.com>
2025-10-18 14:00:00 +0530

    Build login page structure with basic styling

---
+++ client/src/pages/auth/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login to LANE</h2>
+        <p>Welcome back! Please login to your account.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
