30ebe9583b2d7e040dc48cc023c8ad360606226c
mohanganesh3 <mohanganesh165577@gmail.com>
2025-10-18 10:00:00 +0530

    Initialize React project with Vite

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>LANE - Rideshare Platform</h1>
+      <p>React migration in progress...</p>
+    </div>
+  );
+}
+
+export default App;
