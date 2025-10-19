c3c09387c272dd6043c0ba07f5c1996a3bef9382
Akshaya <akshaya.aienavolu@gmail.com>
2025-10-19 10:00:00 +0530

    Add navigation links to header

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <h1 className="logo">LANE</h1>
+        <Link to="/" className="logo">LANE</Link>
+        <nav className="nav">
+          <Link to="/search">Search Rides</Link>
+          <Link to="/post-ride">Post Ride</Link>
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
       </div>
     </header>
   );
